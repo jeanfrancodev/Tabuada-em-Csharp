24c59cc9e1bb7448b8bb519ebb00a6bb5dd54b3c
Jean Franco <jeanoliveirafranco@gmail.com>
2022-07-18 10:21:14 -0300

    tabuada em C#

---
+++ tabuada/tabuada/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+    
+        static void Main(string[] args)
+
+        {
+            int fator, resultado ;
+            Console.WriteLine("Escreva qual numero quer multiplicar");
+            fator = int.Parse(Console.ReadLine());
+
+
+            for (int y = 1; y <= 10; y++)
+            {
+                resultado = fator * y;
+                Console.WriteLine(fator + " x " + y + " = "+ resultado);
+            }
+        }
+    }
+}
